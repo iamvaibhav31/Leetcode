16bc99774f1f155cfca36592dc7822ccc7ec1181
vaibhav  sharma <80534369+iamvaibhav31@users.noreply.github.com>
2021-10-18 00:16:45 +0530

    Add files via upload

---
+++ stack20.py
@@ -0,0 +1,31 @@
+def valid_parentheses(instr,dic):
+    lst = []    
+
+    if (instr[0] == ")") or (instr[0] == "}") or (instr[0] == "]"):
+        return False
+
+    for i in instr:
+        if i in dic.keys():
+            lst.append(i)
+        elif i in dic.values():
+            if len(lst) == 0:
+                return False
+            elif len(lst) != 0:             
+                if (lst[-1] , i) in list(dic.items()):
+                    # print((lst[-1] , i))
+                    lst.pop()
+                elif (lst[-1] , i) not in list(dic.items()):
+                    # print((lst[-1] , i))
+                    return False
+    # print(lst)
+    if len(lst) == 0:
+        return True
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+    dic = {"{":"}","[":"]","(":")"}
+    input_str = str(input()) 
+    print(valid_parentheses(input_str,dic))
